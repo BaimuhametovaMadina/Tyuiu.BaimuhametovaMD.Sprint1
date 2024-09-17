93ef21023f0cd4007dc4d32c656c4bc13a70d72d
BaimukhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-09-17 16:11:57 +0500

    Add project files.

---
+++ Tyuiu.BaimuhametovaMD.Sprint1.Task0.V9.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Tyuiu.BaimuhametovaMadina.Sprint1.Task0.V9.Lib;
+
+namespace Tyuiu.BaimuhametovaMadina.Sprint1.Task0.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.BaimuhametovaMD.Sprint1.Task0.V9/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.BaimuhametovaMadina.Sprint1.Task0.V9.Lib;
+
+// ЗАДАНИЕ
+// Написать программу, которая вычисляет выражение (9/3)*4/3/2+1 и печатает результат на экране.
+
+namespace Tyuiu.BaimuhametovaMadina.Sprint1.Task0.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Баймухаметова М.Д. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("Вариант #9                                                                *");
+            Console.WriteLine("Выполнил: Баймухаметова Мадина | ИБКСб-24-1                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (9/3)*4/3/2+1           *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (9 / 3) * 4 / 3 / 2 + 1                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
