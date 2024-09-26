dfc10242aa549ec140e0017e12f853aaeee1c425
BaimukhametovaMD <Tyuiu.BaimuhametovaMD@gmail.com>
2024-09-26 22:06:37 +0500

    task7

---
+++ Tyuiu.BaimuhametovaMD.Sprint1.Task7.V21/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.BaimuhametovaMD.Sprint1.Task7.V21.Lib;
+
+namespace Tyuiu.BaimuhametovaMD.Sprint1.Task7.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Баймухаметова М.Д. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Баймухаметова Мадина | ИБКСб-24-1                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+            Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РАСЧЁТЫ:                                                                *");
+            Console.WriteLine("* z = (y^x)/(cos(x) - x/3) + ((sin(x^2)+cos(y))/(cos(x)-sin(y)))*tg(x*y)  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("y = " + ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
